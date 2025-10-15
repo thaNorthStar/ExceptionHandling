13796f3098b73d2d8fda068608a2e0d763263f9c
thaNorth$tar <114375239+thaNorthStar@users.noreply.github.com>
2025-10-15 17:07:11 -0500

    Add files via upload

---
+++ ExceptionHandling/src/LaunchException1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LaunchException1 {
+    public static void main(String[] args) throws Exception {
+        // an example of an unchecked exception where
+        // there's no warning from the compiler that
+        // dividing by 0 will result in
+        // java.lang.ArithmeticException: / by zero
+        System.out.println("Connection to app is established");
+        try {
+            Scanner scan=new Scanner(System.in);
+            System.out.println("Enter the 1st number to divide: ");
+            int num1=scan.nextInt();
+            System.out.println("Enter the 2nd number to divide: ");
+            int num2=scan.nextInt();
+            int result=num1/num2;
+            System.out.println("The result is: "+result);
+        } catch (Exception e) {
+            System.out.println("You're trying to divide by zero");;
+        }
+        System.out.println("Connection is terminated");
+    }
+}
